c3878155a0371ab5b057b38b1cc84953ba581c78
rbedi <rbedi100@gmail.com>
2014-02-15 10:44:36 -0500

    back-end making calls to self

--- CloudCode/inputProcessing/cloud/main.js
+++ CloudCode/inputProcessing/cloud/main.js
@@ -1,7 +1,7 @@
-
 var bucketSize = .00005;
 
 var buckets = {};
+
 
 Parse.Cloud.define("storeLocation", function(request, response){
 
@@ -17,6 +17,7 @@ Parse.Cloud.define("storeLocation", function(request, response){
     var bucketName = bucketLat.toString() + "," + bucketLong.toString();
     
     var Bucket = Parse.Object.extend("Bucket");
+
     var query = new Parse.Query(Bucket);
     query.equalTo("bucketName", bucketName);
 
@@ -35,14 +36,14 @@ Parse.Cloud.define("storeLocation", function(request, response){
                 crumbs[userId] = timestamp;
                 bucket.set("crumbs",crumbs);
             }
-            var test = JSON.stringify(crumbs);
-            console.log(test);
             bucket.save(null,{
                 success: function(bucket){
                     bucket.save();
+                    response.success();
                 },
             error: function(bucket, error){
                 alert("failed");
+                response.error(error);
             }
         });
     },
@@ -50,5 +51,98 @@ Parse.Cloud.define("storeLocation", function(request, response){
         alert("Error: "+ error.code + " " + error.message);
     }
     });
- 
+    response.success(); 
+});
+
+
+
+
+Parse.Cloud.define("matchupFinder", function(request, response){  
+
+    Parse.Cloud.useMasterKey();
+
+    var Bucket = Parse.Object.extend("Bucket");
+    var query = new Parse.Query(Bucket);
+   
+    var latitude = request.params.latitude;
+    var longitude = request.params.longitude;
+    var userId = request.params.userId;
+
+    var new_bucket = latitude.toString() + "," + longitude.toString();
+
+    var nearBuckets = new Array();
+
+    for (var i = -1; i <= 1; i++){
+        for (var j = -1; j <= 1; j++){
+            var new_lat = latitude + (.00005 * i);
+            var new_long = longitude + (.00005 * j);
+            var new_bucket = new_lat.toString() + "," + new_long.toString();        
+            console.log(new_bucket); 
+            nearBuckets.push(new_bucket);
+        }
+    }
+    console.log(nearBuckets.toString());
+    console.log(nearBuckets.length.toString());
+    query.containedIn("bucketName", nearBuckets);
+    query.find({
+        success: function(results){
+            for (var i = 0; i < results.length; i++){
+                var bucket = results[i];
+                var hits = bucket.get("crumbs");
+                console.log(hits);
+                
+                /** NEW CODE **/
+                for (var user in hits){
+                   
+                    if (userPair != user){
+                        var userPair = userId + "," + user;
+                        
+                        var Connection = Parse.Object.extend("Connection");
+                        var connection = new Connection;
+
+                        connection.set("userPair", userPair);
+                        connection.set("location", results[i].get("bucketName"));
+                        connection.set("timestamp", hits[user]); 
+
+                        connection.save(null,{
+                            success: function(connection){
+                                console.log("saved.");
+                                connection.save();
+                                response.success();
+                            },
+                            error: function(connection, error){
+                                alert("failed");
+                                response.error();
+                            }
+                        });    
+                    }
+                    else{
+                        response.success();
+                    }
+        
+                }
+
+                /** END NEW CODE **/
+            response.success();
+            }
+           // status.success("woo!");
+        },
+        error: function(){
+           // status.error("fuck.");
+        }
+
+    }); 
+response.success();
+});
+
+
+Parse.Cloud.job("findAllMatchups", function(request, status){
+
+    parameters = new Object();
+    parameters.userId = "erfb9xd1eoahrkhrrdb77woyf";
+    parameters.latitude = 39.9512;
+    parameters.longitude = -75.1934;
+    
+    Parse.Cloud.run("matchupFinder",parameters, {success: function() {status.success("success!")}, error: function(error){ status.error("sad");}});
+        
 });
