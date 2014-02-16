1971a0ff554bbcf70610c81044297c598b8ce023
rbedi <rbedi100@gmail.com>
2014-02-16 03:30:05 -0500

    Working findNumMatches.

--- CloudCode/inputProcessing/cloud/main.js
+++ CloudCode/inputProcessing/cloud/main.js
@@ -6,19 +6,19 @@ var buckets = {};
 
 Parse.Cloud.define("storeLocation", function(request, response){
 
-   // var parseUserId = Parse.User.current().id;
-   // console.log(parseUserId);
+    // var parseUserId = Parse.User.current().id;
+    // console.log(parseUserId);
     var userId = request.params.userId.toString();
-    
+
     var posLat = parseFloat(request.params.posLat);
     var posLong = parseFloat(request.params.posLong);
     var timestamp = request.params.timestamp.toString();
-    
+
     var bucketLat = Math.floor((1.00000/bucketSize) * posLat) / (1.00000 / bucketSize);
     var bucketLong = Math.floor((1.00000/bucketSize) * posLong) / (1.00000 / bucketSize);
 
     var bucketName = bucketLat.toString() + "," + bucketLong.toString();
-    
+
     var Bucket = Parse.Object.extend("Bucket");
 
     var query = new Parse.Query(Bucket);
@@ -26,37 +26,54 @@ Parse.Cloud.define("storeLocation", function(request, response){
 
     query.find({
         success: function(results){
-            if (results.length > 0){
-                var bucket = results[0];
-                var crumbs = bucket.get("crumbs");
-                crumbs[userId] = timestamp;
-                bucket.set("crumbs",crumbs);
-            }
-            else{
-                var bucket = new Bucket;
-                bucket.set("bucketName", bucketName);
-                var crumbs = new Object();
-                crumbs[userId] = timestamp;
-                bucket.set("crumbs",crumbs);
-            }
-            bucket.save(null,{
-                success: function(bucket){
-                    bucket.save();
-                    response.success();
-                },
-            error: function(bucket, error){
-                alert("failed");
-                response.error(error);
-            }
-        });
-    },
-    error: function(error){
-        alert("Error: "+ error.code + " " + error.message);
-    }
+                     if (results.length > 0){
+                         var bucket = results[0];
+                         var crumbs = bucket.get("crumbs");
+                         crumbs[userId] = timestamp;
+                         bucket.set("crumbs",crumbs);
+                     }
+                     else{
+                         var bucket = new Bucket;
+                         bucket.set("bucketName", bucketName);
+                         var crumbs = new Object();
+                         crumbs[userId] = timestamp;
+                         bucket.set("crumbs",crumbs);
+                     }
+    bucket.save(null,{
+        success: function(bucket){
+                     bucket.save();
+                     response.success();
+                 },
+        error: function(bucket, error){
+                   alert("failed");
+                   response.error(error);
+               }
+    });
+                 },
+            error: function(error){
+                       alert("Error: "+ error.code + " " + error.message);
+                   }
     });
 
 });
 
+Parse.Cloud.define("testQuery", function(request, response){
+
+    var Bucket = Parse.Object.extend("Bucket");
+    var query = new Parse.Query(Bucket);
+    query.find({
+        success: function(results){
+                     console.log("success");
+                     console.log(results);
+                 },
+        error: function(error){
+                   console.log(error);
+                   response.error(error);
+               }
+    });
+
+
+});
 
 Parse.Cloud.define("matchupFinder", function(request, response){  
 
@@ -66,10 +83,10 @@ Parse.Cloud.define("matchupFinder", function(request, response){
     var longitude = request.params.longitude;
     var userId = request.params.userId;
 
-    
+
     var Bucket = Parse.Object.extend("Bucket");
     var query = new Parse.Query(Bucket);
-    
+
     console.log("Running " + userId + " on lat " + latitude + " and long " + longitude);
 
     var new_bucket = latitude.toString() + "," + longitude.toString();
@@ -88,50 +105,50 @@ Parse.Cloud.define("matchupFinder", function(request, response){
     console.log(nearBuckets.toString());
     console.log(nearBuckets.length.toString());
     query.containedIn("bucketName", nearBuckets);
-    
+
 
     query.find({
         success: function(results){
-            for (var i = 0; i < results.length; i++){
-                var bucket = results[i];
-                var hits = bucket.get("crumbs");
-                console.log("hits " + hits);
-                
-                for (var user in hits){
-                    if (userId != user){
+                     for (var i = 0; i < results.length; i++){
+                         var bucket = results[i];
+                         var hits = bucket.get("crumbs");
+                         console.log("hits " + hits);
 
-                        var Connection = Parse.Object.extend("Connection");
-                        var connection = new Connection;
+                         for (var user in hits){
+                             if (userId != user){
 
-                        connection.set("user", userId);
-                        connection.set("friend", user);
+                                 var Connection = Parse.Object.extend("Connection");
+                                 var connection = new Connection;
 
-                        connection.set("location", results[i].get("bucketName"));
-                        connection.set("timestamp", hits[user]); 
+                                 connection.set("user", userId);
+                                 connection.set("friend", user);
 
-                        connection.save(null,{
-                            success: function(connection){
-                                console.log("saved.");
-                                connection.save();
-                              
-                            },
-                            error: function(connection, error){
-                                alert("failed");
-                                response.error();
-                            }
-                        });    
-                    }
-                    else{
-                       // 
-                    }
-        
-                }
-            }
-        
-        },
-        error: function(){
-           response.error();
-        }
+                                 connection.set("location", results[i].get("bucketName"));
+                                 connection.set("timestamp", hits[user]); 
+
+                                 connection.save(null,{
+                                     success: function(connection){
+                                                  console.log("saved.");
+                                                  connection.save();
+
+                                              },
+                                     error: function(connection, error){
+                                                alert("failed");
+                                                response.error();
+                                            }
+                                 });    
+                             }
+                             else{
+                                 // 
+                             }
+
+                         }
+                     }
+
+                 },
+            error: function(){
+                       response.error();
+                   }
     }).then(function(){
         response.success();
     }, function(error){
@@ -149,7 +166,7 @@ Parse.Cloud.define("findMatchupsForBucket", function(request, response){
         parameters.userId = user;
         parameters.latitude = loc.split(',')[0];
         parameters.longitude = loc.split(',')[1];
-        
+
         counter += 1;
         console.log("Calling matchupFinder on " + user + " with " + parameters.latitude + " " + parameters.longitude);
         Parse.Cloud.run("matchupFinder",parameters,{success: function() { counter +=1;}, error: function(error){ counter += 1;}});
@@ -162,7 +179,7 @@ Parse.Cloud.job("findAllMatchups", function(request, status){
 
     var Bucket = Parse.Object.extend("Bucket");
     var query = new Parse.Query(Bucket);
-    
+
     var counter = 0;
 
     query.each(function(result){
@@ -170,16 +187,16 @@ Parse.Cloud.job("findAllMatchups", function(request, status){
         parameters = new Object();
         parameters.crumbs = crumbs;
         parameters.loc = result.get("bucketName");
-       Parse.Cloud.run("findMatchupsForBucket",parameters, {success: function(){ counter +=1;}, error: function(error){ counter +=1;}}); 
+        Parse.Cloud.run("findMatchupsForBucket",parameters, {success: function(){ counter +=1;}, error: function(error){ counter +=1;}}); 
 
     }).then(function(){
 
         status.success("success!");
-       
+
     }, function(error){
         status.error("fail");
     });
-               
+
 });
 
 Parse.Cloud.define("findMatchesforUser", function(request, response){
@@ -193,66 +210,66 @@ Parse.Cloud.define("findMatchesforUser", function(request, response){
     query.equalTo("userId", userId);
     query.find({
         success: function(userMatch){
-            
-            if(userMatch.length > 0){
-                userHit = userMatch[0];
-            }
-            else{
-                userHit = new fbUser;
-                userHit.set("userId",userId);
-            }
-            
-           
-            if(userHit.get("hitList")!==undefined){
-                hitList = userHit.get("hitList");
-            }
-            else{
-                hitList = {};
-            }
 
-            var Connection = Parse.Object.extend("Connection");
-            var query = new Parse.Query(Connection);
-            query.equalTo("user",userId);
-            query.find({
-                success: function(results){
-                    for(var i = 0; i < results.length; i++){
-                        var curHit = results[i];
-                        if(curHit.get("friend") in hitList){
-                            hitList[curHit.get("friend")] += 1;
-                        }
-                        else{
-                            hitList[curHit.get("friend")] = 1;
-                        }
-                    }
-                    
-                },
-                error: function(error){
-                    response.error();
-                }
-            });
+                     if(userMatch.length > 0){
+                         userHit = userMatch[0];
+                     }
+                     else{
+                         userHit = new fbUser;
+                         userHit.set("userId",userId);
+                     }
 
 
-            // set and save hitList
+    if(userHit.get("hitList")!==undefined){
+        hitList = userHit.get("hitList");
+    }
+    else{
+        hitList = {};
+    }
 
-            userHit.set("hitList", hitList);
-            userHit.save(null,{
-                success: function(userHit){
-                    console.log("saved.");
-                    userHit.save();
-                  
-                },
-                error: function(userHit, error){
-                    alert("failed");
-                    response.error();
-                }
-            }); 
+    var Connection = Parse.Object.extend("Connection");
+    var query = new Parse.Query(Connection);
+    query.equalTo("user",userId);
+    query.find({
+        success: function(results){
+                     for(var i = 0; i < results.length; i++){
+                         var curHit = results[i];
+                         if(curHit.get("friend") in hitList){
+                             hitList[curHit.get("friend")] += 1;
+                         }
+                         else{
+                             hitList[curHit.get("friend")] = 1;
+                         }
+                     }
 
-
-
-        },
+                 },
         error: function(error){
-            response.error();
-        }
+                   response.error();
+               }
+    });
+
+
+    // set and save hitList
+
+    userHit.set("hitList", hitList);
+    userHit.save(null,{
+        success: function(userHit){
+                     console.log("saved.");
+                     userHit.save();
+
+                 },
+        error: function(userHit, error){
+                   alert("failed");
+                   response.error();
+               }
+    }); 
+
+
+
+                 },
+            error: function(error){
+                       response.error();
+                   }
     });
 
 });
@@ -263,15 +280,56 @@ Parse.Cloud.define("addPhonePair", function(request, response){
     var p2 = request.params.p2;
 
     Parse.Cloud.httpRequest({
-          method: "GET",
-          url: "http://testing.thomasteisberg.com/familiarfaces/addphonepair.php",
-          body: {
-             p1:p1,
-             p2:p2,
-          }
+        method: "GET",
+        url: "http://testing.thomasteisberg.com/familiarfaces/addphonepair.php",
+        body: {
+            p1:p1,
+        p2:p2,
+        }
     });
 
 });
+
+Parse.Cloud.define("getMatchName", function(request, response){
+    var userId = request.params.userId;
+    var threshold = request.params.threshold;
+    var hits = new Array();
+    var fbUser = Parse.Object.extend("fbUser");
+    var query = new Parse.Query(fbUser);
+    query.equalTo("userId", userId);
+    query.find({
+        success: function(userMatch){
+                     if(userMatch.length > 0){
+                         userHit = userMatch[0];
+                         hitList = userHit.get("hitList");
+                         for(var friend in hitList){
+                             if(hitList[friend] > threshold){
+                                 hits.push(friend);
+                             }
+                         }
+                         var randHit = hits[Math.floor(Math.random() * hits.length)];
+                     }    
+                 },
+        error: function(error){
+                   response.error();
+               }
+    });
+
+    response.success(randHit);
+
+});
+
+Parse.Cloud.define("rejectMatch", function(request, response){
+    // passing in the userId of the match (as matchUserId) and current userId
+    // add each person to the other's exclusion list
+    // no response
+});
+
+Parse.Cloud.define("inviteChat", function(request,response){
+    // passing in userId of match (as matchUserId) and current userId
+    // needs to send push notification to other user *****
+});
+
 
 Parse.Cloud.define("sms", function(request, response){
 
@@ -282,8 +340,8 @@ Parse.Cloud.define("sms", function(request, response){
         method: "GET",
         url: "http://testing.thomasteisberg.com/familiarfaces/sms.php",
         body: {
-            p1:p1,
-            msg:msg,
+            msisdn:p1,
+        text:msg,
         }
 
     });
@@ -292,25 +350,50 @@ Parse.Cloud.define("sms", function(request, response){
 Parse.Cloud.define("findNumMatches", function(request, response){
     var userId = request.params.userId;
     var threshold = request.params.threshold;
+    var fbFriends = request.params.friends;
     var counter = 0;
-    var fbUser = Parse.Object.Extend("fbUser");
+
+    var fbUser = Parse.Object.extend("fbUser");
     var query = new Parse.Query(fbUser);
     query.equalTo("userId", userId);
+    
+    console.log("about to query for user");
     query.find({
-        success: function(results){
-            var curUser = results[0];
-            var hitList = curUser.get("hitList");
-            for(var friend in hitList){
-                if(hitList[friend] > threshold){
-                    counter++;
-                }
-            }
-        },
-        error: function(error){
-            response.error();
-        }
-        });
+        success: function(result){
+                     if(result.length > 0){
+                        var curUser = results[0];
+                        var hitList = curUser.get("hitlist");
+                        for(var friend in hitList){
+                            if(hitList[friend] > threshold){
+                                counter++;
+                            }
+                        }
+                     }
+                     else{
+                        curUser = new fbUser;
+                        curUser.set("userId", userId);
+                        counter = 0;
+                        curUser.set("exclusionList", fbFriends);
+                        
+                        curUser.save(null,{
+                            success:function(curUser){
+                                        curUser.save();
+                                    },
+                            error: function(curUser, error){
+                                   response.error(error);
+                                   }
+                        });
+                     }
 
+        
+                     },
+        error: function(error){
+                   response.error(error);
+               }
+
+}).then(function(){
     response.success(counter);
-  
+}, function(error){
+    response.error();
+});
 });
