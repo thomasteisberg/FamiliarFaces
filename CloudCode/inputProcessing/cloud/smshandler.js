e988a4c724887ee08a95f818803ef0f4a153cd72
Thomas Teisberg <teisberg@stanford.edu>
2014-02-15 19:19:11 -0800

    Response code 200 from smsrx

--- CloudCode/inputProcessing/cloud/smshandler.js
+++ CloudCode/inputProcessing/cloud/smshandler.js
@@ -1,5 +1,8 @@
 
 Parse.Cloud.define("smsrx", function(request, response){
 	console.log('got sms');
-	response.success('test');
+	var jsonObject = {
+    		"code": 200
+  	};
+	response.success(jsonObject);
 });
